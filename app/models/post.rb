1263b2b2d6bfdd544a3c2b284b19aa1abd1db4ca
nathan hains <nathanhains14@yahoo.com>
2021-04-14 17:54:28 -0400

    commit tree 5dd3bdb23005bc4c60caef0aebae7fa171bf55f4

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-
-
+  accepts_nested_attributes_for :categories
+  def categories_attributes=(category_attributes)
+    category_attributes.each do |i, category_attributes|
+      if category_attributes[:name] != ""
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        self.post_categories.build(category: category)
+      end
+    end
+  end
 end
